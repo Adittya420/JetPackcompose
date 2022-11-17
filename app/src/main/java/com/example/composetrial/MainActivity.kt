844bdbab3583c564774764aee806b6bb3ad3bccd
Aditya Arakharao <ararakharao_b21@it.vjti.ac.in>
2022-11-17 18:32:20 +0530

    Scrollable list with normal column

--- app/src/main/java/com/example/composetrial/MainActivity.kt
+++ app/src/main/java/com/example/composetrial/MainActivity.kt
@@ -3,9 +3,8 @@ package com.example.composetrial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.rememberScrollableState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -23,6 +22,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -39,32 +40,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val scaffoldState = rememberScaffoldState()
-            var textFieldState by remember() {
-                 mutableStateOf(" ")
-            }
-            val scope = rememberCoroutineScope()
-            Scaffold(modifier = Modifier.fillMaxSize()
-            , scaffoldState = scaffoldState ) {
-                Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center, modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 30.dp)) {
-                     TextField(value = textFieldState,
-                     label = {
-                         Text("Enter Name")
-                     }, onValueChange = {
-                         textFieldState=it
-                         }, singleLine = true, modifier = Modifier.fillMaxWidth())
-                    Spacer(modifier = Modifier.height(20.dp))
-                    Button(onClick = { /*TODO*/
-                    scope.launch{
-                        scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState")
-                    }}) {
-                        Text(text = "Greet Me")
-                    }
+            val scroll = rememberScrollState()
+            Column (modifier = Modifier.verticalScroll(scroll)){
+                for (i in 1..50){
+                    Text(
+                        text = "Item number $i",
+                        fontSize= 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 20.dp)
+                    )
                 }
             }
-          
         }
     }
 }
