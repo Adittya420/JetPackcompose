6bcd5e02b49a2e9ab694f64b573d7a06f539e9e6
Aditya Arakharao <ararakharao_b21@it.vjti.ac.in>
2022-11-19 17:22:42 +0530

    Added CircularProgression Animation

--- app/src/main/java/com/example/composetrial/MainActivity.kt
+++ app/src/main/java/com/example/composetrial/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.composetrial
 
+import android.os.Build.VERSION_CODES.S
 import android.os.Bundle
+import android.transition.CircularPropagation
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.spring
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.rememberScrollableState
 import androidx.compose.foundation.layout.*
@@ -20,6 +19,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layoutId
@@ -31,8 +32,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.*
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
@@ -44,36 +44,68 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 import java.net.ContentHandler
+import kotlin.time.Duration
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-            var sizecon by remember {
-                mutableStateOf(200.dp)
-            }
-            val size by animateDpAsState(targetValue = sizecon ,
-            spring(
-                Spring.DampingRatioLowBouncy
-            )
-            )
-
-
-            Box(
-                modifier = Modifier
-                    .size(size)
-                    .background(Color.Red).fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                Button(onClick = { /*TODO*/
-                sizecon+=50.dp}) {
-                    Text(text = "Click Me")
-                }
+            Box (
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+                    ){
+                CircularPropagation(percentage = 0.9f , number = 100)
             }
 
-            
             }
         }
     }
+
+@Composable
+fun CircularPropagation(
+    percentage :Float,
+    number:Int,
+    fontsize : TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color:Color = Color.Green,
+    strokeWith: Dp = 8.dp,
+    animDuration: Int = 1000,
+    animDelay : Int = 0
+    ){
+var animPlayed by remember {
+    mutableStateOf(false)
+}
+    val currper = animateFloatAsState(targetValue = if(animPlayed) percentage else 0f, animationSpec = tween(
+        durationMillis = animDuration,
+        delayMillis = animDelay
+    ))
+    LaunchedEffect(key1 = true){
+        animPlayed = true
+    }
+    Box (
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f)
+            ){
+        Canvas(modifier = Modifier.size(radius*2f) ){
+            drawArc(
+                color = Color.Green,
+                -90f,
+                360 * currper.value,
+                useCenter = false,
+                style = Stroke(strokeWith.toPx(),cap = StrokeCap.Round),
+            )
+
+        }
+        Text(
+            text = (currper.value*number).toInt().toString(),
+            color=Color.Red,
+            fontSize = fontsize
+        )
+
+    }
+   
+
+
+}
+
 
