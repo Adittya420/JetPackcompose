c798aa36892697326a5bb385555965be6b65798d
Aditya Arakharao <ararakharao_b21@it.vjti.ac.in>
2022-11-22 12:05:29 +0530

    Made a Toolbar

--- app/src/main/java/com/example/composetrial/MainActivity.kt
+++ app/src/main/java/com/example/composetrial/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.composetrial
 
+import android.graphics.Paint.Cap
 import android.os.Build.VERSION_CODES.S
 import android.os.Bundle
 import android.transition.CircularPropagation
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.rememberScrollableState
@@ -12,18 +14,28 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PointMode
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -41,71 +53,61 @@ import kotlin.random.Random.Default.nextFloat
 
 import com.example.composetrial.ui.theme.ComposeTrialTheme
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 import java.net.ContentHandler
+import java.util.Timer
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 import kotlin.time.Duration
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box (
-                contentAlignment = Alignment.Center,
-                modifier = Modifier.fillMaxSize()
-                    ){
-                CircularPropagation(percentage = 0.9f , number = 100)
-            }
+            Scaffold(topBar = {
+                TopAppBar(
+                    title={
+                        Text("JetPackcompose")
+                    },
+                    navigationIcon = {
+                                     IconButton(onClick = { /*TODO*/ }) {
+                                         Icon(Icons.Filled.Menu, contentDescription = "My Menu")
+                                     }
+                    },
+                    actions = {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(Icons.Filled.Notifications , contentDescription = "Noti")
+                        }
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(Icons.Filled.Search , contentDescription = "Search")
+                        }
 
+                    }
+
+
+                )
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Filled.Add, contentDescription = "Add")
+                }
+            }) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Hello Android",
+                    )
+                }
+
+                
             }
         }
     }
-
-@Composable
-fun CircularPropagation(
-    percentage :Float,
-    number:Int,
-    fontsize : TextUnit = 28.sp,
-    radius: Dp = 50.dp,
-    color:Color = Color.Green,
-    strokeWith: Dp = 8.dp,
-    animDuration: Int = 1000,
-    animDelay : Int = 0
-    ){
-var animPlayed by remember {
-    mutableStateOf(false)
-}
-    val currper = animateFloatAsState(targetValue = if(animPlayed) percentage else 0f, animationSpec = tween(
-        durationMillis = animDuration,
-        delayMillis = animDelay
-    ))
-    LaunchedEffect(key1 = true){
-        animPlayed = true
-    }
-    Box (
-        contentAlignment = Alignment.Center,
-        modifier = Modifier.size(radius * 2f)
-            ){
-        Canvas(modifier = Modifier.size(radius*2f) ){
-            drawArc(
-                color = Color.Green,
-                -90f,
-                360 * currper.value,
-                useCenter = false,
-                style = Stroke(strokeWith.toPx(),cap = StrokeCap.Round),
-            )
-
-        }
-        Text(
-            text = (currper.value*number).toInt().toString(),
-            color=Color.Red,
-            fontSize = fontsize
-        )
-
-    }
-   
-
-
 }
 
 
